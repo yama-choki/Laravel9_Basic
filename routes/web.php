0b8d851374593d80ba156f79cde0d906104a75ae
yama-choki <lovepingpong0@gmail.com>
2022-10-25 16:53:16 +0900

    教材112　削除機能追加、JSで確認を出した後削除する

--- routes/web.php
+++ routes/web.php
@@ -29,6 +29,7 @@ Route::prefix('contacts') //prefixをつけると以降のグループ内の各�
     Route::get('/{id}', 'show')->name('show');
     Route::get('/{id}/edit', 'edit')->name('edit');
     Route::post('/{id}', 'update')->name('update');
+    Route::post('/{id}/destroy', 'destroy')->name('destroy');
 });
 
 Route::get('/', function () {
